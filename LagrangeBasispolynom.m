6ef931489976dea1d6779a85a7a2a7008351103e
Felix Voelker <s9442559@NPLP32.pool.math.tu-dresden.de>
2015-11-20 13:17:49 +0100

    obvious bugs/spelling fixed

--- LagrangeBasispolynom.m
+++ LagrangeBasispolynom.m
@@ -1,9 +1,10 @@
 function L_j = LagrageBasispolynom(j,var,x)
-  double top = 1, souble bottom
+  double top = 1, double bottom
   for i = 1:size(x)
-    if i/=j+1 then
-      top = top * (var - x(i))
-      bottom = bottom*(x(j+1)-x(i))
+    if i~=j+1; then
+      top = top * (var - x(i));
+      bottom = bottom*(x(j+1)-x(i));
     end
   end
+  L_j = top/bottom;
 end  
