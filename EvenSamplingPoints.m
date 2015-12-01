28f572c3be273a2cf0c7639f10688a123a5d9dd1
FelixUni <Felix.voelker@tu-dresden.de>
2015-12-01 17:35:44 +0100

    fixed EvenSamplingPoints.m

--- EvenSamplingPoints.m
+++ EvenSamplingPoints.m
@@ -2,7 +2,7 @@ function [ X_N ] = EvenSamplingPoints( N )
 %EVENSAMPLINGPOINTS gives evenly spaced sampling points
 %   in the intervall I = [-1,1] N evenly spaced sampling points will be
 %   returned, reference: (A)
-X_N=-1:N:1;
+X_N=-1:(2/N):1;
 
 
 end
