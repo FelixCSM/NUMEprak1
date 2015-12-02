9dce1b0986f3425de31aa6a0e999fced49bcf815
FelixCSM <felix-voelker@web.de>
2015-12-02 17:31:28 +0100

    interpolation plots

--- MSamplings.m
+++ MSamplings.m
@@ -2,12 +2,12 @@ function [ M ] = MSamplings( samples , N )
 %MSAMPLINGS implements the 10th-fold decomposition
 %   Explaination is reference to Aufgabe
 
-if samples == @EvenSamplingPoints
-    M=-1:5*N:1;
+if isequal(samples,@EvenSamplingPoints)
+    M=-1:1/(5*N):1;
 end
-if samples == @TscherbySamples
+%if samples == TscherbySamples
     
-end
+%end
 
 
 end
--- plot_g_N.m
+++ plot_g_N.m
@@ -2,8 +2,9 @@ function [ A ] = plot_g_N( func, samps, N )
 %PLOT_G_N Implements g_N
 %@param function to plot and amount of sampling points in I
 %@return vector with a_i for the plotted polynomial
-A=polyfit(samps(N),arrayfun(func,samps(N)),N+1);
-plot(samps(N),polyval(A,samps(N)))
+A=polyfit(samps(N),arrayfun(func,samps(N)),N);
+figure();
+plot(MSamplings(samps,N),polyval(A,MSamplings(samps,N)))
 
 
 end
