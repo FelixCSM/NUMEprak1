73b08ecb8c2fd9d1e2f529f57bf9e1d1a694160d
FelixCSM <felix-voelker@web.de>
2015-12-02 19:32:48 +0100

    plot Tscherby has type mistakes, besides that all is plotting

--- MSamplings.m
+++ MSamplings.m
@@ -6,9 +6,16 @@ if isequal(samples,@EvenSamplingPoints)
     M=-1:1/(5*N):1;
 end
 if isequal(samples, @TscherbySamples)
-    
+    M = array(10*(N+2));
+    M(1) = -1;          %Extensions of somple values
+    M(size(M)) = 1;     %---------"----------------- 
+    T = TscherbySamples(N);
+    for k = 1:N+1
+       M(k*10) = T(k);           
+       for i = 1:9
+           M(k*10+i) = (M((k+1)*10)-M(k*10))*(i/10);
+       end
+    end
 end
-
-
 end
 
