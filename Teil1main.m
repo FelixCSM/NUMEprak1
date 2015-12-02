86559cd06c8cb77b59f9aceec72ef407275f7c56
FelixCSM <felix-voelker@web.de>
2015-12-02 15:12:08 +0100

    expanded by different SamplingPoints

--- Teil1main.m
+++ Teil1main.m
@@ -1,8 +1,13 @@
 %Main script for Teil1
 N = input('What is N?\n');
 %part (i)
-plot_g_N(@f1,N)
-plot_g_N(@RungeFunction,N)
+plot_g_N(@f1,@EvenSamplingPoints ,N)
+plot_g_N(@f1 ,@TscherbySamples ,N)
+
+plot_g_N(@RungeFunction,@EvenSamplingPoints,N)
+plot_g_N(@RungeFunction,@TscherbySamples,N)
+
+
 %part(iii)
 plot_F_N(@f1,N)
 plot_F_N(@RungeFunction,N)
