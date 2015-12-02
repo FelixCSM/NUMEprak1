c59f5f487980dc87783b37887d8da8cead08a553
FelixCSM <felix-voelker@web.de>
2015-12-02 20:42:32 +0100

    little fix in counting var

--- MSamplings.m
+++ MSamplings.m
@@ -6,14 +6,14 @@ if isequal(samples,@EvenSamplingPoints)
     M=-1:1/(5*N):1;
 end
 if isequal(samples, @TscherbySamples)
-    M = array(10*(N+2));
+    M = 1:(10*(N+2)+1);
     M(1) = -1;          %Extensions of somple values
-    M(size(M)) = 1;     %---------"----------------- 
-    T = TscherbySamples(N);
+    M(10*(N+2)+1) = 1;     %---------"----------------- 
+    T = samples(N);
     for k = 1:N+1
-       M(k*10) = T(k);           
+       M(k*10+1) = T(k);           
        for i = 1:9
-           M(k*10+i) = (M((k+1)*10)-M(k*10))*(i/10);
+           M((k-1)*10+i) = (M(k*10+1)-M((k-1)*10+1))*(i/10);
        end
     end
 end
