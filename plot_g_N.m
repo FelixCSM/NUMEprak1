2ba654a344bc0e36fdb2ba828177668165a32fae
FelixCSM <felix-voelker@web.de>
2015-12-02 15:12:42 +0100

    implemented plot_g_N.m

--- plot_g_N.m
+++ plot_g_N.m
@@ -1,6 +1,9 @@
-function [ output_args ] = plot_g_N( input_args )
-%PLOT_G_N Summary of this function goes here
-%   Detailed explanation goes here
+function [ A ] = plot_g_N( func, samps, N )
+%PLOT_G_N Implements g_N
+%@param function to plot and amount of sampling points in I
+%@return vector with a_i for the plotted polynomial
+A=polyfit(samps(N),arrayfun(@func,samps(N)),N);
+
 
 
 end
