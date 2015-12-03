eba5dac9d515919724bf9ebc8120b985ceaa3966
FelixCSM <felix-voelker@web.de>
2015-12-03 19:44:21 +0100

    all but F_N of Teil1 is done

--- MSamplings.m
+++ MSamplings.m
@@ -6,16 +6,18 @@ if isequal(samples,@EvenSamplingPoints)
     M=-1:1/(5*N):1;
 end
 if isequal(samples, @TscherbySamples)
-    M = 1:(10*(N+2)+1);
-    M(1) = -1;          %Extensions of somple values
-    M(10*(N+2)+1) = 1;     %---------"----------------- 
-    T = samples(N);
-    for k = 1:N+1
-       M(k*10+1) = T(k);           
-       for i = 1:9
-           M((k-1)*10+i) = (M(k*10+1)-M((k-1)*10+1))*(i/10);
-       end
-    end
+   T =  [-1 TscherbySamples(N) 1]; 
+   M(1)=T(1);
+   for i = 1:length(T)-1
+       M(10*(i-1)+1:10*(i))=tenthes(T(i),T(i+1));
+   end
 end
+end
+
+function [ x_tilde ] = tenthes( x_0 ,x_1)
+%takes two values and returns a matrix of lenth = 10 with even entrys
+%between the parameters
+
+x_tilde = x_0+(x_1-x_0)/10:(x_1-x_0)/10:x_1;
 end
 
